c4dd267cdd3de117b37d7690484a89cce0d172ce
Brian Diesel <bri@ndiesel.net>
2024-09-25 19:46:16 -0400

    Updating a simple test change to test xcode SC

--- checkSplit/ContentView.swift
+++ checkSplit/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Test Change!")
+            Text("Hello World!")
         }
         .padding()
     }
